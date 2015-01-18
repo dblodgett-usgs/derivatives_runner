3c2e99a68cd0bae3cb1965915d47f2554c3824e5
David Blodgett <dblodgett@usgs.gov>
2015-01-17 21:18:16 -0700

    finished up the first cut of the script.

--- periodizer.R
+++ periodizer.R
@@ -2,8 +2,13 @@ library(ncdf4)
 library(dapClimates)
 library(chron)
 
+# Previously run derivatives.
 data_path<-'/Users/dblodgett/temp/cmip5_der/'
+
+# Where to write the results.
 out_path<-'/Users/dblodgett/temp/cmip5_der_periods/'
+
+# Thresholds used with previously run derivatives.
 thresholds=list(days_tmax_abv_thresh=c(32.2222,35,37.7778),
                 days_tmin_blw_thresh=c(-17.7778,-12.2222,0),
                 days_prcp_abv_thresh=c(25.4,50.8,76.2,101.6),
@@ -14,11 +19,15 @@ thresholds=list(days_tmax_abv_thresh=c(32.2222,35,37.7778),
                 cooling_degree_day_thresh=c(18.3333),
                 growing_season_lngth_thresh=c(0))
 
+# Units from previously run derivatives.
 p_units<-'mm/d'
 t_units<-'C'
 
-periods<-c(2011,2040,2070,2100)
+# Periods that will be used. These should include n+1 values where n is the number 
+# of actual time steps desired. The last vaue should be the end of the final time step.
+periods<-c(2011,2040,2070,2099)
 
+# Generate filename list based on thresholds, note this logic is kinda redundant. See test below.
 fileNames<-c()
 for (stat in names(thresholds))
 {
@@ -27,38 +36,59 @@ for (stat in names(thresholds))
   fileNames<-append(fileNames,fileName)
 }
 
-setwd(data_path)
-gcm_scenarios<-list.dirs()
+# Set working directory and get scenario names from it. Just being lazy here.
+setwd(data_path); gcm_scenarios<-list.dirs()
 
-# Loop over gcm_scenarios
+# Will implement a loop over gcm_scenarios here. Should be 2:length(gcm_scenarios)
 gcm_scenario<-tail(unlist(strsplit(gcm_scenarios[2],'/')),n=1)
 
 #Use the first file for creation of the output ones.
 nc_file<-fileNames[1]
-dir.create(paste(out_path,gcm_scenario,sep=''),showWarnings=FALSE)
-setwd(paste(out_path,gcm_scenario,sep=''))
-OPeNDAP_URI<-paste(data_path,gcm_scenario,'/',nc_file,sep='')
-tryCatch(ncid <- nc_open(OPeNDAP_URI), error = function(e) 
+# Create output directory and set it as the working directory.
+dir.create(paste(out_path,gcm_scenario,sep=''),showWarnings=FALSE); setwd(paste(out_path,gcm_scenario,sep=''))
+
+# Try and open the file.
+tryCatch(ncid <- nc_open(paste(data_path,gcm_scenario,'/',nc_file,sep='')), error = function(e) 
 {
   cat("An error was encountered trying to open the OPeNDAP resource."); print(e)
 })
-x_vals<-ncid$dim$lon$vals
-y_vals<-ncid$dim$lat$vals
+
+# Initialize the output NetCDF files.
+x_vals<-ncid$dim$lon$vals; y_vals<-ncid$dim$lat$vals
 li<-initialize_NetCDF(ncid, thresholds, start=FALSE, end=FALSE, tmax_var=FALSE, prcp_var=FALSE, x_vals, y_vals, periods, t_units, p_units)
 out_filenames<-li$fileNames
-# Should probably make sure the in and out filenames are the same!
-# if(any(out_filenames!=fileNames)) 
+
+# Make sure the in and out filenames are the same!
+stopifnot(any(out_filenames==fileNames))
+
+# Clean up
+nc_close(ncid)
 
 # Loop over fileNames
+for(file in out_filenames) {
+  ncid_out<-nc_open(file,write=TRUE)
+  ncid_in<-nc_open(paste(data_path,gcm_scenario,'/',file,sep=''))
+  var_id<-unlist(strsplit(tail(unlist(strsplit(file,'/')),n=1),'[.]'))[1]
+  var_data <- ncvar_get(ncid_in, varid=var_id)
+  if(length(dim(var_data))==4) out_data<-array(1, dim=c(nrow(var_data),ncol(var_data),dim(var_data)[3],length(periods)-1))
+  else out_data<-array(1, dim=c(nrow(var_data),ncol(var_data),length(periods)-1))
+  for(per_ind in 1:(length(periods)-1)){
+    li<-request_time_bounds(ncid_in,periods[per_ind],periods[per_ind+1])
+    start_ind<-li$t_ind1; end_ind<-li$t_ind2; time_inds<-li$time
+    if(length(dim(var_data))==4) out_data[,,,per_ind]<-round(apply(var_data[,,,start_ind:end_ind],c(1,2,3),mean, na.rm = TRUE),digits=2)
+    else out_data[,,per_ind]<-round(apply(var_data[,,start_ind:end_ind], c(1,2), mean, na.rm = TRUE),digits=2)
+  }
+  out_data[is.nan(out_data)]<--1
+  ncvar_put(ncid_out,var_id,out_data)
+  ncvar_put(ncid_out,'threshold',ncvar_get(ncid_in,'threshold'))
+  nc_close(ncid_in)
+  nc_close(ncid_out)
+}
 
 
-var_id<-unlist(strsplit(tail(unlist(strsplit(nc_file,'/')),n=1),'[.]'))[1]
 
-var_data <- ncvar_get(ncid, varid=var_id, start=c(1,1,1,1), count=c(-1,-1,-1,-1))
 
-li<-request_time_bounds(ncid,'2011','2040')
-start_ind<-li$t_ind1
-end_ind<-li$t_ind2
-time_inds<-li$time
+
+
 
 
