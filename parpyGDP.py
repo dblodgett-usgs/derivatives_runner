87875d4df2516e265690d90ea369a20d1f53457e
David L Blodgett <dblodgett@usgs.gov>
2015-07-28 14:12:56 -0500

    ONLY DO THIS AGAINST LOCAL HOST!!!

---
+++ parpyGDP.py
@@ -0,0 +1,23 @@
+#!/Volumes/Scratch/dblodgett_workspace/pyGDP/venv/bin/python
+import os
+import subprocess
+import time
+import shlex
+processes = []
+max_processes = 15
+pause_time=2
+file_processing = 1
+while file_processing<104:
+    command=shlex.split('/Volumes/Scratch/dblodgett_workspace/pyGDP/venv/bin/python /Volumes/Scratch/dblodgett_workspace/derivatives_runner/pyGDP_runner.py')
+    print str(file_processing)+ ' of ' + str(104)
+    file_processing+=1
+    processes.append(subprocess.Popen(command))
+    if len(processes) < max_processes:
+        time.sleep(pause_time)
+    while len(processes) >= max_processes:
+        time.sleep(pause_time*2)
+        processes = [proc for proc in processes if proc.poll() is None]
+
+while len(processes) > 0:
+    time.sleep(pause_time)
+    processes = [proc for proc in processes if proc.poll() is None]
