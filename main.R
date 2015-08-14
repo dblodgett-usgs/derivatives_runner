e2fcf53875f3e953325dc2d54412c3138f3e37e6
David L Blodgett <dblodgett@usgs.gov>
2015-08-14 11:48:08 -0500

    getting paths right in the pyGDP runner

--- main.R
+++ main.R
@@ -3,6 +3,7 @@ library(derivativesRunner)
 # Set script constants
 storage_root<-'/Volumes/Scratch/thredds/bcca/bcca/cmip5' # Where all the raw data is.
 out_root<-'/Volumes/Striped/final_derivatives' # Where data will get written to durring processing.
+package_files<-'/Volumes/Scratch/dblodgett_workspace/derivatives_runner' # There's probably a better way.
 bbox_in<-c(-67.06,52.81,-124.6,25.18) # bbox of the data.
 cpus<-25
 
@@ -12,14 +13,14 @@ derivatives_runner_fun(storage_root, out_root, bbox_in, cpus)
 # User this python
 py_virt_env<-'/Volumes/Scratch/dblodgett_workspace/pyGDP/venv/bin/python'
 
-ncml_path<-file.path(out_root,'ncml')
-ncml_script<-file.path(find.package(package=derivativesRunner),'python','ncml_script.py')
+ncml_path<-file.path(out_root,'derivatives','ncml')
+ncml_script<-file.path(package_files,'python','ncml_script.py')
 dir.create(ncml_path, recursive = TRUE)
 # Create ncml for output data.
 system(paste(py_virt_env, ncml_script, ncml_path))
 
-pyGDP_runner<-file.path(find.package(package=derivativesRunner),'python','parpyGDP.py')
-pyGDP_script<-file.path(find.package(package=derivativesRunner),'python','pyGDP_runner.py')
+pyGDP_runner<-file.path(package_files,'python','parpyGDP.py')
+pyGDP_script<-file.path(package_files,'python','pyGDP_runner.py')
 
 gdp_output_root<-file.path(out_root,'pyGDP_output/run')
 dir.create(gdp_output_root, recursive = TRUE)
--- python/pyGDP_runner.py
+++ python/pyGDP_runner.py
@@ -15,31 +15,31 @@ shapefiles={'derivative:CONUS_States':'STATE',
             'derivative:NCA_Regions':'NCA_Region',
             'derivative:wbdhu8_alb_simp':'HUC_8'}
           
-dataURIs=['http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/longest_run_tmax_abv_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/longest_run_prcp_blw_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/heating_degree_day_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/growing_season_lngth_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/growing_degree_day_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/days_tmin_blw_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/days_tmax_abv_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/days_prcp_abv_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/cooling_degree_day_cmip5_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/longest_run_tmax_abv_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/longest_run_prcp_blw_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/heating_degree_day_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/growing_season_lngth_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/growing_degree_day_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/days_tmin_blw_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/days_tmax_abv_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/days_prcp_abv_cmip5_hist_der.ncml',
-          'http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/cooling_degree_day_cmip5_hist_der.ncml']
+dataURIs=['http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/longest_run_tmax_abv_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/longest_run_prcp_blw_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/heating_degree_day_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/growing_season_lngth_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/growing_degree_day_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/days_tmin_blw_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/days_tmax_abv_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/days_prcp_abv_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/cooling_degree_day_cmip5_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/longest_run_tmax_abv_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/longest_run_prcp_blw_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/heating_degree_day_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/growing_season_lngth_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/growing_degree_day_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/days_tmin_blw_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/days_tmax_abv_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/days_prcp_abv_cmip5_hist_der.ncml',
+          'http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/cooling_degree_day_cmip5_hist_der.ncml']
 
 for dataURI in dataURIs:
-    remote_dataURI=dataURI.replace('http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/','http://cida.usgs.gov/thredds/dodsC/cmip5_bcca/derivatives/ncml/')
+    remote_dataURI=dataURI.replace('http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/','http://cida.usgs.gov/thredds/dodsC/cmip5_bcca/derivatives/ncml/')
     dataTypes = pyGDP.getDataType(remote_dataURI)
     timeRange = pyGDP.getTimeRange(remote_dataURI, dataTypes[1])
     for shapefile in shapefiles.keys():
-        outputfilename=shapefile.replace('derivative:','')+'_'+dataURI.replace('http://localhost:8080/thredds/dodsC/Striped/derivatives/ncml/','')
+        outputfilename=shapefile.replace('derivative:','')+'_'+dataURI.replace('http://localhost:8080/thredds/dodsC/Striped/final_derivatives/derivatives/ncml/','')
         if not os.path.isfile(outputfilename):
             open(outputfilename, 'a').close()
             print shapefile
